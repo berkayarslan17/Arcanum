6481754e2ff3f1d5f6bb57831257a9ef5193f352
Berkay Arslan <berkay.arslan@bordatech.com>
2025-10-28 19:25:25 +0300

    feat: add simple hash table implementation

---
+++ DataStructures/hash_table.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define NAME_SIZE 20
+#define TABLE_SIZE 20
+
+typedef struct {
+    char name[NAME_SIZE];
+    unsigned int age;
+    unsigned int height;
+} person_t;
+
+person_t *hash_table[TABLE_SIZE];
+
+static unsigned int hash(const char *p_name)
+{
+    unsigned int len = strnlen(p_name, NAME_SIZE);
+    unsigned int hash = 0;
+
+    for(int i = 0; i < len; i++)
+    {
+        hash += p_name[i];
+        hash = (hash * p_name[i]) % TABLE_SIZE;
+    }
+
+    return hash;
+}
+
+void hash_table_init(void)
+{
+    for(int i = 0; i < TABLE_SIZE; i++)
+    {
+        hash_table[i] = NULL;
+    }
+}
+
+bool hash_table_insert_person(const person_t *p_person)
+{
+    bool is_success = true;
+    unsigned int idx = hash(p_person->name);
+
+    if(hash_table[idx] == NULL)
+    {
+        hash_table[idx] = p_person;
+    }
+    else
+    {
+        is_success = false;
+    }
+
+    return is_success;
+}
+
+bool hash_table_delete_person(const person_t *p_person)
+{
+    bool is_success = true;
+    unsigned int idx = hash(p_person->name);
+
+    if(hash_table[idx] != NULL && !memcmp(hash_table[idx]->name, p_person->name, NAME_SIZE))
+    {
+        hash_table[idx] = NULL;
+    }
+    else
+    {
+        is_success = false;
+    }
+
+    return is_success;
+}
+
+void hash_table_print(void)
+{
+    printf("HASH TABLE\n");
+
+    for(int i = 0; i < TABLE_SIZE; i++)
+    {
+        printf("Idx: %d\t\t\tAddress:%p\t\n", i, hash_table[i]);
+    }
+}
+
+int main()
+{
+    person_t person1 = {
+        .name = "Berkay",
+        .age = 26,
+        .height = 191
+    };
+
+    person_t person2 = {
+        .name = "Fatih",
+        .age = 26,
+        .height = 181
+    };
+
+    hash_table_init();
+
+    hash_table_insert_person(&person1);
+    hash_table_print();
+    hash_table_insert_person(&person2);
+    hash_table_print();
+    hash_table_delete_person(&person1);
+    hash_table_delete_person(&person2);
+    hash_table_print();
+}
